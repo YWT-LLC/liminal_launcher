bfa73a4c6fb931dd5f950c43e02cd86047e8c0e6
empathictech <105748722+empathictech@users.noreply.github.com>
2025-09-28 11:40:52 -0700

    adding shake for feedback

--- lib/widgets/liminal_scaffold.dart
+++ lib/widgets/liminal_scaffold.dart
@@ -4,11 +4,14 @@
  */
 
 import './export.dart';
+import '../utils/consts.dart';
+import 'package:efui_bios/efui_bios.dart';
 
+import 'package:shake/shake.dart';
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
-class LiminalScaffold extends StatelessWidget {
+class LiminalScaffold extends StatefulWidget {
   /// [Scaffold.body] passthrough
   final Widget body;
 
@@ -20,17 +23,61 @@ class LiminalScaffold extends StatelessWidget {
   const LiminalScaffold(this.body, {super.key, this.fabs});
 
   @override
-  Widget build(BuildContext context) => EzAdaptiveScaffold(
-        small: Scaffold(
-          body: body,
-          floatingActionButton: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[updater, if (fabs != null) ...fabs!],
-          ),
-          floatingActionButtonLocation: EzConfig.get(isLeftyKey)
-              ? FloatingActionButtonLocation.startFloat
-              : FloatingActionButtonLocation.endFloat,
-          resizeToAvoidBottomInset: false,
+  State<LiminalScaffold> createState() => _LiminalScaffoldState();
+}
+
+class _LiminalScaffoldState extends State<LiminalScaffold> {
+  ShakeDetector? detector;
+
+  @override
+  void initState() {
+    super.initState();
+    if (EzConfig.get(shakeForFeedbackKey)) startDetector();
+  }
+
+  void startDetector() {
+    detector?.stopListening();
+    detector = ShakeDetector.autoStart(
+      onPhoneShake: (_) => ezFeedback(
+        parentContext: context,
+        l10n: ezL10n(context),
+        supportEmail: empathSupport,
+        appName: appName,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (EzConfig.get(shakeForFeedbackKey)) {
+      ShakeDetector.autoStart(
+        onPhoneShake: (_) => ezFeedback(
+          parentContext: context,
+          l10n: ezL10n(context),
+          supportEmail: empathSupport,
+          appName: appName,
         ),
       );
+    }
+
+    return EzAdaptiveScaffold(
+      small: Scaffold(
+        body: widget.body,
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[updater, if (widget.fabs != null) ...widget.fabs!],
+        ),
+        floatingActionButtonLocation: EzConfig.get(isLeftyKey)
+            ? FloatingActionButtonLocation.startFloat
+            : FloatingActionButtonLocation.endFloat,
+        resizeToAvoidBottomInset: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    detector?.stopListening();
+    super.dispose();
+  }
 }
