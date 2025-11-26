05d02b54ec9eb868fbfdab91fcc4a9c7fd881c21
empathictech <105748722+empathictech@users.noreply.github.com>
2025-11-26 11:15:14 -0800

    ripple -> anim

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -6,8 +6,7 @@
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
-/// [ezAnimDuration] mod 1.5
-final Duration rippleDuration = ezAnimDuration(mod: 1.5);
+final Duration animDuration = ezAnimDuration();
 
 /// Tracks the position of the last ripple LongPress
 /// Defaults to [Offset.zero]
