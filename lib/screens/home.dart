4812bca0dd8009cb3fda2bdc4913b1f06a8ebf0e
empathictech <105748722+empathictech@users.noreply.github.com>
2025-12-03 13:54:23 -0800

    fixed scroll notifs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -356,6 +356,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: EzScrollView(
                           mainAxisAlignment: vAlign.mainAxis,
                           crossAxisAlignment: hAlign.crossAxis,
+                          physics: const ClampingScrollPhysics(),
                           children: homeTiles,
                         ),
                       ),
