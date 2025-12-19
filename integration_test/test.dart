09cfcfacef2a5996fb4b640eab9d8537f2d2a0a1
empathictech <105748722+empathictech@users.noreply.github.com>
2025-12-19 00:26:40 -0800

    new config init

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -18,14 +18,13 @@ void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
 
-  SharedPreferences.setMockInitialValues(empathMobileConfig);
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-
+  final SharedPreferencesAsync preferences = SharedPreferencesAsync();
   EzConfig.init(
-    preferences: prefs,
+    assetPaths: <String>{},
     defaults: liminalDefault,
     fallbackLang: await EFUILang.delegate.load(americanEnglish),
-    assetPaths: <String>{},
+    preferences: preferences,
+    storedKeys: await preferences.getKeys(),
   );
 
   // Run the tests //
