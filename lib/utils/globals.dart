17ff324e5b92dbc20364dc756c7b044805a818d1
empathictech <105748722+empathictech@users.noreply.github.com>
2026-03-14 11:54:53 -0400

    removed unneeded

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -3,27 +3,8 @@
  * See LICENSE for distribution and usage details.
  */
 
-import './export.dart';
-
 import 'package:flutter/material.dart';
-import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
-
-/// Lots of custom animations in Liminal, having a global final makes things easier
-final Duration animDuration = ezAnimDuration();
 
 /// Tracks the position of the last ripple LongPress
 /// Defaults to [Offset.zero]
 Offset lastRipple = Offset.zero;
-
-/// [FloatingActionButton] list for the settings screens
-List<Widget> settingsFABs(BuildContext context, {bool home = false}) =>
-    <Widget>[
-      EzConfig.spacer,
-      EzConfigFAB(
-        context,
-        appName: appName,
-        androidPackage: androidPackage,
-      ),
-      EzConfig.spacer,
-      EzBackFAB(showHome: home),
-    ];
