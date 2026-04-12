929c24f06759b17dc9f4f990609cf4a82036e50a
empathictech <105748722+empathictech@users.noreply.github.com>
2026-04-12 10:06:45 -0400

    local classes

--- lib/screens/settings/home.dart
+++ lib/screens/settings/home.dart
@@ -15,196 +15,181 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 class SettingsHomeScreen extends StatelessWidget {
   SettingsHomeScreen() : super(key: ValueKey<int>(EzConfig.seed));
 
-  // Return the build //
-  // TODO: buttonify stuff, lose settings are overwhelming!
+  @override
+  Widget build(BuildContext context) => LiminalScaffold(
+        Center(
+          child: EzScrollView(children: <Widget>[
+            const _HeaderSettings(),
+            EzConfig.spacer,
+            const _SwipeSettings(),
+            EzConfig.spacer,
+            const _AppListSettings(),
+            EzConfig.spacer,
+            const EzThemeCoin(),
+            EzConfig.divider,
+            EzElevatedIconButton(
+              onPressed: () => context.goNamed(appearanceSettingsPath),
+              icon: const Icon(Icons.navigate_next),
+              label: 'Appearance settings',
+            ),
+          ]),
+        ),
+        fabs: settingsFABs(context, home: true),
+      );
+}
+
+class _HeaderSettings extends StatelessWidget {
+  const _HeaderSettings();
 
   @override
   Widget build(BuildContext context) {
-    final DateTime now = DateTime.now();
+    return EzElevatedIconButton(
+      label: 'Home header',
+      icon: const Icon(LineIcons.clock),
+      onPressed: () => ezModal(
+        context: context,
+        builder: (BuildContext modalContext) => EzScrollView(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // Hide status bar
+            EzSwitchPair(
+              // TODO: either make work, or add snack to tell them to restart
+              text: 'Hide status bar',
+              valueKey: hideStatusKey,
+              afterChanged: (_) => EzConfig.rebuildUI(doNothing),
+            ),
+            EzConfig.spacer,
 
-    return LiminalScaffold(
-      EzScrollView(children: <Widget>[
-        EzHeader(),
+            // Home Time
+            EzSwitchPair(
+              text: 'Show time',
+              valueKey: EzConfig.isDark ? darkHomeTimeKey : lightHomeTimeKey,
+            ),
+            EzConfig.spacer,
 
-        // Swipe selectors
-        EzElevatedIconButton(
-          label: 'Quick launch',
-          icon: const Icon(Icons.swipe),
-          onPressed: () => ezModal(
-            context: context,
-            builder: (_) => EzScrollView(
+            // Home Date
+            // TODO: add checks on close for redrawUI (rebuild shouldn't be needed)
+            EzScrollView(
+              scrollDirection: Axis.horizontal,
+              reverseHands: true,
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                Text(
-                  'On the home screen, swiping left/right will open the selected app:',
-                  textAlign: TextAlign.center,
+                // Label
+                EzText(
+                  'Date type',
                   style: EzConfig.styles.bodyLarge,
+                  textAlign: TextAlign.center,
                 ),
-                EzConfig.spacer,
-                const SwipeSelector(left: true),
-                EzConfig.spacer,
-                const SwipeSelector(left: false),
-                EzConfig.separator,
+                EzConfig.margin,
+
+                // Button
+                EzDropdownMenu<DateType>(
+                  enableSearch: false,
+                  initialSelection: homeDate,
+                  dropdownMenuEntries: DateType.values
+                      .map((DateType type) => DropdownMenuEntry<DateType>(
+                          value: type,
+                          label: DateTypeConfig.buildDate(
+                            modalContext,
+                            DateTime.now(),
+                            type,
+                          )))
+                      .toList(),
+                  widthEntries: <String>['Wednesday, Sept'],
+                  onSelected: (DateType? choice) async {
+                    if (choice == null) return;
+
+                    if (EzConfig.updateBoth || EzConfig.isDark) {
+                      await EzConfig.setString(darkHomeDateKey, choice.value);
+                    }
+                    if (EzConfig.updateBoth || !EzConfig.isDark) {
+                      await EzConfig.setString(lightHomeDateKey, choice.value);
+                    }
+                  },
+                ),
               ],
             ),
-          ),
+            EzConfig.separator,
+          ],
         ),
-        EzConfig.spacer,
-
-        // Home header
-        EzElevatedIconButton(
-          label: 'Home header',
-          icon: const Icon(LineIcons.clock),
-          onPressed: () => ezModal(
-            context: context,
-            builder: (_) => EzScrollView(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                // Hide status bar
-                EzSwitchPair(
-                  text: 'Hide status bar',
-                  valueKey: hideStatusKey,
-                  afterChanged: (_) => EzConfig.rebuildUI(doNothing),
-                ),
-                EzConfig.spacer,
-
-                // Home Time
-                EzSwitchPair(
-                  text: 'Show time',
-                  valueKey:
-                      EzConfig.isDark ? darkHomeTimeKey : lightHomeTimeKey,
-                ),
-                EzConfig.spacer,
-
-                // Home Date
-                EzScrollView(
-                  scrollDirection: Axis.horizontal,
-                  reverseHands: true,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    // Label
-                    EzText(
-                      'Date type',
-                      style: EzConfig.styles.bodyLarge,
-                      textAlign: TextAlign.center,
-                    ),
-                    EzConfig.margin,
-
-                    // Button
-                    EzDropdownMenu<DateType>(
-                      enableSearch: false,
-                      initialSelection: homeDate,
-                      dropdownMenuEntries: DateType.values
-                          .map((DateType type) => DropdownMenuEntry<DateType>(
-                                value: type,
-                                label: DateTypeConfig.buildDate(
-                                    type, context, now),
-                              ))
-                          .toList(),
-                      widthEntries: <String>['Wednesday, Sept'],
-                      onSelected: (DateType? choice) async {
-                        if (choice == null) return;
-
-                        if (EzConfig.updateBoth || EzConfig.isDark) {
-                          await EzConfig.setString(
-                              darkHomeDateKey, choice.value);
-                        }
-                        if (EzConfig.updateBoth || !EzConfig.isDark) {
-                          await EzConfig.setString(
-                              lightHomeDateKey, choice.value);
-                        }
-
-                        await EzConfig.redrawUI(doNothing);
-                      },
-                    ),
-                  ],
-                ),
-                EzConfig.separator,
-              ],
-            ),
-          ),
-        ),
-        EzConfig.spacer,
-
-        // Auto add to home
-        const EzSwitchPair(
-          text: 'Add new apps to home',
-          valueKey: autoAddToHomeKey,
-        ),
-        EzConfig.spacer,
-
-        // Auto search
-        const EzSwitchPair(text: 'Auto search', valueKey: autoSearchKey),
-        EzConfig.separator,
-
-        // Auth to edit
-        const EzSwitchPair(text: 'Auth to edit', valueKey: authToEditKey),
-        EzConfig.spacer,
-
-        // Auth for hidden
-        const EzSwitchPair(text: 'Auth for hidden', valueKey: authForHiddenKey),
-        EzConfig.divider,
-
-        // Appearance
-        EzElevatedIconButton(
-          onPressed: () => context.goNamed(appearanceSettingsPath),
-          icon: const Icon(Icons.navigate_next),
-          label: 'Appearance settings',
-        ),
-        EzConfig.separator,
-      ]),
-      fabs: settingsFABs(context, home: true),
+      ),
     );
   }
 }
 
-// TODO: find me a new home
-// @override
-//   void afterFirstLayout(BuildContext context) async {
-//     if (!(await isGPlayInstall()) &&
-//         !EzConfig.get(shownReminderKey) &&
-//         context.mounted) {
-//       await showDialog(
-//         context: context,
-//         builder: (_) => EzAlertDialog(
-//           title: const Text(
-//             'Welcome to Liminal Launcher',
-//             textAlign: TextAlign.center,
-//           ),
-//           content: EzRichText(
-//             <InlineSpan>[
-//               const EzPlainText(
-//                 text:
-//                     '''We hope it serves you well! This version is not from the Play Store, so it should have been free.
-// Rest assured, the free version of Liminal will always be identical to the Google Play version.
+class _SwipeSettings extends StatelessWidget {
+  // TODO: dark/light split
+  const _SwipeSettings();
 
-// With that said, if you want to support Liminal's development, or the development of more Empathetech software, please consider ''',
-//               ),
-//               EzInlineLink(
-//                 'contributing',
-//                 style: EzConfig.styles.bodyLarge,
-//                 textAlign: TextAlign.center,
-//                 url: Uri.parse('https://www.empathetech.net/#/contribute'),
-//                 hint: 'Open a link to the Empathetic contribution options.',
-//               ),
-//               const EzPlainText(
-//                 text: '''.
+  @override
+  Widget build(BuildContext context) => EzElevatedIconButton(
+        label: 'Quick launch',
+        icon: const Icon(Icons.swipe),
+        onPressed: () => ezModal(
+          context: context,
+          builder: (_) => EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Swipe left/right on the home screen (not in editing mode) to open the selected app.',
+                textAlign: TextAlign.center,
+                style: EzConfig.styles.bodyLarge,
+              ),
+              EzConfig.spacer,
+              const SwipeSelector(left: true),
+              EzConfig.spacer,
+              const SwipeSelector(left: false),
+              EzConfig.separator,
+            ],
+          ),
+        ),
+      );
+}
 
-// This is the only non-tutorial dialog that will appear.
-// And it will not appear again.
+class _AppListSettings extends StatelessWidget {
+  const _AppListSettings();
 
-// Thank you, and enjoy!''',
-//               ),
-//             ],
-//             style: EzConfig.styles.bodyLarge,
-//             textBackground: false,
-//             textAlign: TextAlign.center,
-//           ),
-//         ),
-//       );
-//       await EzConfig.setBool(shownReminderKey, true);
-//     }
-//   }
+  @override
+  Widget build(BuildContext context) => EzElevatedIconButton(
+        label: 'App list',
+        icon: const Icon(Icons.list),
+        onPressed: () => ezModal(
+          context: context,
+          builder: (_) => EzScrollView(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              // Auto add to home
+              const EzSwitchPair(
+                text: 'Auto-add new apps to home',
+                valueKey: autoAddToHomeKey,
+              ),
+              EzConfig.spacer,
+
+              // Auto search
+              const EzSwitchPair(
+                text: 'Auto-search the apps list',
+                valueKey: autoSearchKey,
+              ),
+              EzConfig.separator,
+
+              // Auth to edit
+              const EzSwitchPair(
+                text: 'Auth to edit lists/settings',
+                valueKey: authToEditKey,
+              ),
+              EzConfig.spacer,
+
+              // Auth for hidden
+              const EzSwitchPair(
+                text: 'Auth to see hidden apps',
+                valueKey: authForHiddenKey,
+              ),
+              EzConfig.separator,
+            ],
+          ),
+        ),
+      );
+}
 
 // TODO: Find me a new home
 // Future<dynamic> showTips() => showDialog(
