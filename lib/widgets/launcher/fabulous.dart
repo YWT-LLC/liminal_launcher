ad19020c98aaf8fd059d33464af3d080dc0f6f17
empathictech <105748722+empathictech@users.noreply.github.com>
2026-04-26 16:03:35 -0400

    moved

--- lib/widgets/launcher/fabulous.dart
+++ lib/widgets/launcher/fabulous.dart
@@ -3,7 +3,7 @@
  * See LICENSE for distribution and usage details.
  */
 
-import '../screens/export.dart';
+import '../../screens/export.dart';
 
 import 'package:flutter/material.dart';
 import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
