f472d0114f304543842bc738f2d9e2c45c63d7c3
empathictech <105748722+empathictech@users.noreply.github.com>
2026-04-26 16:04:09 -0400

    organized and added button

--- lib/widgets/export.dart
+++ lib/widgets/export.dart
@@ -3,13 +3,14 @@
  * See LICENSE for distribution and usage details.
  */
 
-export 'home/app_folder.dart';
-export 'home/app_tile.dart';
-export 'home/clock.dart';
+export 'launcher/app_folder.dart';
+export 'launcher/app_tile.dart';
+export 'launcher/button.dart';
+export 'launcher/clock.dart';
+export 'launcher/fabulous.dart';
 
 export 'settings/alignment.dart';
 export 'settings/swipe_selector.dart';
 export 'settings/tiles.dart';
 
-export 'fabulous.dart';
 export 'liminal_scaffold.dart';
