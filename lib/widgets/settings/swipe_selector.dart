51b5401d7c2cc58e992135bb591fb6f32494abac
empathictech <105748722+empathictech@users.noreply.github.com>
2026-06-11 06:47:12 -0400

    renamed

--- lib/widgets/settings/swipe_selector.dart
+++ lib/widgets/settings/swipe_selector.dart
@@ -69,7 +69,7 @@ class _SwipeSelectorState extends State<SwipeSelector> {
           onPressed: () => context.pushNamed(
             appListPath,
             extra: ListConfig(
-              contents: <ListContent>{ListContent.banished},
+              listContent: <ListContent>{ListContent.banished},
               include: false,
               onSelected: (String id) async {
                 final AppInfo? newApp = widget.appInfo.appMap[id];
