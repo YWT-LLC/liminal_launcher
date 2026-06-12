8d7beb6ffce396383afd03aceca61a481c70f8f8
empathictech <105748722+empathictech@users.noreply.github.com>
2026-06-12 07:48:59 -0400

    using cache

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -8,6 +8,36 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 import './export.dart';
 import 'package:local_auth/local_auth.dart';
 
+Future<void> canEdit(EzCP config, Future<void> Function() onSuccess) async {
+  if (!authToEdit(config)) {
+    await onSuccess.call();
+    return;
+  }
+
+  bool authed = false;
+  try {
+    authed = await liminalAuth(config, 'Authenticate to edit the launcher');
+  } catch (e) {
+    ezLog(e.toString());
+  }
+
+  if (authed) await onSuccess.call();
+}
+
+Future<bool> liminalAuth(EzCP config, String reason) async {
+  final String lastAuth = await EzCM.secGet(lastAuthKey);
+
+  // Check quick exit(s)
+  if (lastAuth.isEmpty || authTimeout(config) <= Duration.zero) return _externalAuth(reason);
+
+  // Do the math
+  final DateTime? saved = DateTime.tryParse(lastAuth);
+
+  return (saved == null || DateTime.now().difference(saved) > authTimeout(config))
+      ? _externalAuth(reason)
+      : Future<bool>.value(true);
+}
+
 Future<bool> _externalAuth(String reason) async {
   final bool authed = await LocalAuthentication().authenticate(
     localizedReason: reason,
@@ -17,20 +47,4 @@ Future<bool> _externalAuth(String reason) async {
 
   if (authed) await EzCM.secSet(lastAuthKey, DateTime.now().toString());
   return authed;
-}
-
-Future<bool> liminalAuth(String reason) async {
-  final int timeout =
-      int.tryParse(await EzCM.secGet(authTimeoutKey)) ?? (limSecDef[authTimeoutKey] as int);
-  final String lastAuth = await EzCM.secGet(lastAuthKey);
-
-  // Check quick exit(s)
-  if (timeout <= 0 || lastAuth.isEmpty) return _externalAuth(reason);
-
-  // Do the math
-  final DateTime? saved = DateTime.tryParse(lastAuth);
-
-  return (saved == null || DateTime.now().difference(saved) > Duration(minutes: timeout))
-      ? _externalAuth(reason)
-      : Future<bool>.value(true);
 }
