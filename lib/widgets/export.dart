dfb537532237ca2ff311f82bc7cd1884211ad009
empathictech <105748722+empathictech@users.noreply.github.com>
2026-06-18 14:29:57 -0400

    more custom widgets

--- lib/widgets/export.dart
+++ lib/widgets/export.dart
@@ -3,12 +3,10 @@
  * See LICENSE for distribution and usage details.
  */
 
-export 'launcher/app_tile.dart';
-export 'launcher/clock.dart';
-export 'launcher/fabulous.dart';
-export 'launcher/folder_tile.dart';
-export 'launcher/spacer.dart';
-export 'launcher/widgets.dart';
+export 'lists/app_tile.dart';
+export 'lists/fabulous.dart';
+export 'lists/folder_tile.dart';
+export 'lists/spacer.dart';
 
 export 'settings/alignment.dart';
 export 'settings/app_list.dart';
@@ -16,5 +14,12 @@ export 'settings/app_sec.dart';
 export 'settings/header.dart';
 export 'settings/swipe_selector.dart';
 export 'settings/tiles.dart';
+
+export 'widget-ception/clock.dart';
+export 'widget-ception/create_calendar.dart';
+export 'widget-ception/search.dart';
+export 'widget-ception/stopwatch.dart';
+export 'widget-ception/timer.dart';
+export 'widget-ception/toggle_media.dart';
 
 export 'liminal_scaffold.dart';
