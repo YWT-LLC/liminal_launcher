646ed30258af27b235d916d9bc32a7918396ea48
empathictech <105748722+empathictech@users.noreply.github.com>
2026-06-18 22:07:43 -0400

    saving progress

--- lib/widgets/widget-ception/search.dart
+++ lib/widgets/widget-ception/search.dart
@@ -12,23 +12,57 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 // TODO: short and long press for everything
 // TODO: have a tool tipper on the add screen that describes what it does ahead of time
 // TODO: at least two, preferably three sizes for everything
+// TODO: can I use voice to text? if yes, let the user override long press on the icon button for insta voice...
+// ...they will still be able to edit the widget in editing mode
 
 class SearchWidget extends StatelessWidget {
   final EzCP config;
   late final WidgetSize _size;
   final AppState state;
+  late final TextEditingController _queryCon;
 
   SearchWidget(this.config, WidgetSize size, this.state, {super.key}) {
     _size = (size == WidgetSize.system) ? bt2WS(config) : size;
+    _queryCon = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) => switch (_size) {
         WidgetSize.system => const SizedBox.shrink(), // override above
-        WidgetSize.button || _ => EzIconButton(
+        WidgetSize.button => EzIconButton(
             config,
             icon: EzIcon(config, Icons.search),
             onPressed: () => launchUrl(Uri.parse('https://duckduckgo.com/')),
           ),
+        WidgetSize.tile => elevatedLists(config)
+            ? EzIconButton(
+                config,
+                icon: EzRow(
+                  config,
+                  children: <Widget>[
+                    ConstrainedBox(
+                      constraints: ezTextFieldConstraints(context),
+                      child: TextFormField(controller: _queryCon),
+                    ),
+                    config.rowMargin,
+                    EzIconButton(
+                      config,
+                      icon: EzIcon(config, Icons.search),
+                      onPressed: () => launchUrl(_queryCon.text.trim().isEmpty
+                          ? Uri.parse('https://duckduckgo.com/')
+                          : Uri.https(
+                              'duckduckgo.com',
+                              '/',
+                              <String, dynamic>{'q': _queryCon.text.trim()},
+                            )),
+                    ),
+                  ],
+                ),
+              )
+            : EzRow(
+                config,
+                children: <Widget>[],
+              ),
+        WidgetSize.unbound => const SizedBox.shrink(), // TODO
       };
 }
