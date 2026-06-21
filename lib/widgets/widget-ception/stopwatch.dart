21bd0d66186be45101641b972f227814c5221884
empathictech <105748722+empathictech@users.noreply.github.com>
2026-06-20 21:45:32 -0400

    ye, not much to do hear tbh

--- lib/widgets/widget-ception/stopwatch.dart
+++ lib/widgets/widget-ception/stopwatch.dart
@@ -17,28 +17,19 @@ class StopwatchWidget extends StatelessWidget {
   final int index;
   final AppState state;
 
-  late final WidgetSize _size;
-  late final List<String>? _extra;
-  late final Future<void> Function(String, bool) _save;
-
-  StopwatchWidget(this.config, this.appInfo, this.lane, this.index, this.state, {super.key}) {
-    final List<String> data = appInfo.homeList(config, lane)[index].split(widgetSplit);
-
-    final WidgetSize size = WSConfig.lookup(data[1]);
-    _size = (size == WidgetSize.system) ? bt2WS(config) : size;
-
-    _extra = data.length > 2 ? data.sublist(2) : null;
-
-    _save = (_, __) async {};
-  }
+  const StopwatchWidget(
+    this.config,
+    this.appInfo,
+    this.lane,
+    this.index,
+    this.state, {
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) => switch (_size) {
-        WidgetSize.system => const SizedBox.shrink(), // override above
-        WidgetSize.button || _ => EzIconButton(
-            config,
-            icon: EzIcon(config, Icons.watch),
-            onPressed: openStopwatch,
-          ),
-      };
+  Widget build(BuildContext context) => EzIconButton(
+        config,
+        icon: EzIcon(config, Icons.watch),
+        onPressed: openStopwatch,
+      );
 }
