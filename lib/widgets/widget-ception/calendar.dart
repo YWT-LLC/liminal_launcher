4b7663af24ce087a1ced0a1b08476e1594b82a8e
empathictech <105748722+empathictech@users.noreply.github.com>
2026-06-20 21:42:44 -0400

    saving progress

--- lib/widgets/widget-ception/calendar.dart
+++ lib/widgets/widget-ception/calendar.dart
@@ -10,7 +10,7 @@ import 'package:empathetech_flutter_ui/empathetech_flutter_ui.dart';
 
 // TODO: states, ripples, and edits
 
-class CalendarWidget extends StatelessWidget {
+class CalendarWidget extends StatefulWidget {
   final EzCP config;
   final AppInfoProvider appInfo;
   final int lane;
@@ -18,25 +18,60 @@ class CalendarWidget extends StatelessWidget {
   final AppState state;
 
   late final WidgetSize _size;
-  late final List<String>? _extra;
-  late final Future<void> Function(String, bool) _save;
 
   CalendarWidget(this.config, this.appInfo, this.lane, this.index, this.state, {super.key}) {
     final List<String> data = appInfo.homeList(config, lane)[index].split(widgetSplit);
 
     final WidgetSize size = WSConfig.lookup(data[1]);
     _size = (size == WidgetSize.system) ? bt2WS(config) : size;
-
-    _extra = data.length > 2 ? data.sublist(2) : null;
-
-    _save = (_, __) async {};
   }
 
   @override
-  Widget build(BuildContext context) => switch (_size) {
+  State<CalendarWidget> createState() => _CalendarWidgetState();
+}
+
+class _CalendarWidgetState extends State<CalendarWidget> {
+  // Define the build data //
+
+  late final TextEditingController eventCon;
+  OverlayEntry? overlayEntry;
+
+  // Define custom functions //
+
+  void onChanged() {
+    final String text = eventCon.text.trim();
+
+    (text.isNotEmpty)
+        ? ((overlayEntry == null) ? showOverlay() : overlayEntry?.markNeedsBuild())
+        : removeOverlay();
+  }
+
+  void showOverlay() {
+    overlayEntry = textFormOverlay(widget.config, eventCon.text);
+    ezRootNav.currentState?.overlay?.insert(overlayEntry!);
+  }
+
+  void removeOverlay() {
+    overlayEntry?.remove();
+    overlayEntry = null;
+  }
+
+  // Init //
+
+  @override
+  void initState() {
+    super.initState();
+    eventCon = TextEditingController();
+    eventCon.addListener(onChanged);
+  }
+
+  // Return the build //
+
+  @override
+  Widget build(BuildContext context) => switch (widget._size) {
         WidgetSize.button => EzIconButton(
-            config,
-            iconSize: appIconSize(config),
+            widget.config,
+            iconSize: appIconSize(widget.config),
             icon: const Icon(Icons.edit_calendar),
             onPressed: () async {
               final bool success = await createCalendarEvent();
@@ -45,7 +80,7 @@ class CalendarWidget extends StatelessWidget {
                 await showDialog(
                   context: context,
                   builder: (_) => EzAlertDialog(
-                    config,
+                    widget.config,
                     title: const Text('Failed', textAlign: TextAlign.center),
                     content: const Text(
                       "There likely isn't a default calendar app.\nShall I self-destruct?",
@@ -53,23 +88,23 @@ class CalendarWidget extends StatelessWidget {
                     ),
                     actions: <Widget>[
                       EzMaterialAction(
-                        config,
+                        widget.config,
+                        text: 'No',
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                      EzMaterialAction(
+                        widget.config,
                         text: 'Yes',
                         onPressed: () async {
                           Navigator.of(context).pop();
-                          await ezNoTouch(() async => await appInfo.deleteWidget(
-                                config,
-                                lane: lane,
-                                index: index,
+                          await ezNoTouch(() async => await widget.appInfo.deleteWidget(
+                                widget.config,
+                                lane: widget.lane,
+                                index: widget.index,
                               ));
                         },
                         isDestructiveAction: true,
                         isDefaultAction: true,
-                      ),
-                      EzMaterialAction(
-                        config,
-                        text: 'No',
-                        onPressed: () => Navigator.of(context).pop(),
                       ),
                     ],
                     needsClose: false,
@@ -78,11 +113,47 @@ class CalendarWidget extends StatelessWidget {
               }
             },
           ),
-        _ => EzIconButton(
-            config,
-            iconSize: appIconSize(config),
-            icon: const Icon(Icons.edit_calendar),
-            onPressed: createCalendarEvent,
-          ),
+        _ => EzRow(
+            widget.config,
+            children: <Widget>[
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: ezTextSize(
+                        'Search bar',
+                        context: context,
+                        style: widget.config.bodyStyle,
+                      ).width +
+                      widget.config.padding,
+                  maxHeight: appIconSize(widget.config),
+                ),
+                child: NotificationListener<ScrollNotification>(
+                  // Block scroll notifications
+                  onNotification: (ScrollNotification notification) => true,
+                  child: TextFormField(
+                    controller: eventCon,
+                    decoration: const InputDecoration(hintText: 'Event'),
+                    textAlign: TextAlign.center,
+                    textAlignVertical: TextAlignVertical.center,
+                    onFieldSubmitted: (_) => createCalendarEvent, // TODO: actually uses text
+                  ),
+                ),
+              ),
+              widget.config.rowMargin,
+              EzIconButton(
+                widget.config,
+                icon: const Icon(Icons.edit_calendar),
+                iconSize: appIconSize(widget.config),
+                onPressed: createCalendarEvent,
+              ),
+            ],
+          )
       };
+
+  @override
+  void dispose() {
+    eventCon.removeListener(onChanged);
+    eventCon.dispose();
+    removeOverlay();
+    super.dispose();
+  }
 }
