272f75cdc57b1d4b98d8f1511ae923bb799bbf71
empathictech <105748722+empathictech@users.noreply.github.com>
2026-07-09 16:59:19 -0400

    not needed, in efui now

--- lib/widgets/export.dart
+++ lib/widgets/export.dart
@@ -13,7 +13,6 @@ export 'lists/spacer.dart';
 export 'settings/alignment.dart';
 export 'settings/app_list.dart';
 export 'settings/app_sec.dart';
-export 'settings/form_field.dart';
 export 'settings/lane.dart';
 export 'settings/swipe_selector.dart';
 export 'settings/tiles.dart';
