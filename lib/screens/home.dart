1e8ecfafa8243f54f584b9118205627820d41727
empathictech <105748722+empathictech@users.noreply.github.com>
2026-07-11 13:32:22 -0400

    typo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,7 +3,7 @@
  * See LICENSE for distribution and usage details.
  */
 
-// TODO: you an save the home config, but upload doesn't seem to work
+// TODO: you can save the home config, but upload doesn't seem to work
 // TODO: make the save/load here save everything by default, and have the settings page on be a toggle to save everything (default only appearance stuff)
 
 import '../screens/export.dart';
