30ba3b93ca037a00b5008702fff56f302c81bda6
empathictech <105748722+empathictech@users.noreply.github.com>
2026-07-24 15:57:20 -0400

    restored engine choices

--- lib/widgets/widget-ception/search.dart
+++ lib/widgets/widget-ception/search.dart
@@ -18,7 +18,7 @@ class SearchWidget extends StatefulWidget {
   final EzCP config;
   final AppInfoProvider appInfo;
   final LimPos pos;
-  final AppState state;
+  final TileState state;
   final ValueNotifier<double>? rippleProgress;
 
   late final WidgetSize _size;
@@ -86,7 +86,7 @@ class SearchWidget extends StatefulWidget {
 class _SearchWidgetState extends State<SearchWidget> {
   // Define the build data //
 
-  late AppState state = widget.state;
+  late TileState state = widget.state;
   Timer? rippleThrottle;
 
   final MenuController menuControl = MenuController();
@@ -109,8 +109,8 @@ class _SearchWidgetState extends State<SearchWidget> {
     if (dy <= (widget.rippleProgress!.value * heightOf(context))) {
       setState(
         () => state = switch (state) {
-          AppState.standard => AppState.groupEdit,
-          _ => AppState.standard,
+          TileState.standard => TileState.groupEdit,
+          _ => TileState.standard,
         },
       );
 
@@ -218,7 +218,7 @@ class _SearchWidgetState extends State<SearchWidget> {
       forceFade: true,
       forceType: EzTransitionType.none,
       child: switch (state) {
-        AppState.standard => MenuAnchor(
+        TileState.standard => MenuAnchor(
             builder: (_, MenuController controller, __) => (widget._size == WidgetSize.button)
                 ? EzIconButton(
                     widget.config,
@@ -262,6 +262,7 @@ class _SearchWidgetState extends State<SearchWidget> {
               state: state,
               numLanes: numLanes,
               pos: widget.pos,
+              engineChoices: engineChoices,
               initConfig: _SearchConfig(
                 size: widget._size,
                 engine: widget._engine,
@@ -280,6 +281,7 @@ class _SearchWidgetState extends State<SearchWidget> {
               state: state,
               numLanes: numLanes,
               pos: widget.pos,
+              engineChoices: <Widget>[],
               initConfig: _SearchConfig(
                 size: widget._size,
                 engine: widget._engine,
@@ -308,12 +310,16 @@ List<Widget> _menuChildren(
   EzCP config, {
   required AppInfoProvider appInfo,
   required BuildContext context,
-  required AppState state,
+  required TileState state,
   required int numLanes,
   required LimPos pos,
+  required List<Widget> engineChoices,
   required _SearchConfig initConfig,
 }) =>
     <Widget>[
+      // Engines
+      ...engineChoices,
+
       // Edit
       _EditSearch(
         config,
@@ -348,7 +354,7 @@ List<Widget> _menuChildren(
       ),
 
       // Move
-      if (state == AppState.groupEdit && numLanes > 1) ...<Widget>[
+      if (state == TileState.groupEdit && numLanes > 1) ...<Widget>[
         moveDownLane(config, appInfo, numLanes: numLanes, lane: pos.lane, index: pos.index),
         moveUpLane(config, appInfo, numLanes: numLanes, lane: pos.lane, index: pos.index),
       ],
