03c33b0171e6975ddfd538b186bf5a1f2153352b
empathictech <105748722+empathictech@users.noreply.github.com>
2026-07-25 19:44:58 -0400

    added label to edit button

--- lib/widgets/lists/spacer.dart
+++ lib/widgets/lists/spacer.dart
@@ -734,6 +734,7 @@ class _EditSpacer extends StatelessWidget {
             index: pos.index,
           );
         },
+        label: 'Edit',
         icon: EzIcon(config, Icons.edit),
       );
 }
