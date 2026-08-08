c604e7254e6bf9417e9f8c04f9f62a9903c96ee7
empathictech <105748722+empathictech@users.noreply.github.com>
2026-08-08 11:08:44 -0400

    using new-ish pos

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -502,8 +502,7 @@ const Duration breatheTime = Duration(seconds: 2);
 
 Offset lastRipple = Offset.zero;
 
-final ValueNotifier<(int?, int?)> marked = ValueNotifier<(int?, int?)>((null, null));
-bool get editingSpacer => marked.value.$1 != null || marked.value.$2 != null;
+final ValueNotifier<LimPos?> marked = ValueNotifier<LimPos?>(null);
 
 final ValueNotifier<double> editSpacerHeight = ValueNotifier<double>(defaultMobileSpacing);
 final ValueNotifier<double> editSpacerWidth = ValueNotifier<double>(defaultMobileSpacing);
