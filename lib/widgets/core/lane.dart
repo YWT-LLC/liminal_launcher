4cd749376250a195b2253167773f1bfa4a164388
empathictech <105748722+empathictech@users.noreply.github.com>
2026-08-28 13:15:45 -0400

    note to self

--- lib/widgets/core/lane.dart
+++ lib/widgets/core/lane.dart
@@ -13,6 +13,8 @@ import 'package:go_router/go_router.dart';
 
 //* Core 'Widget' *//
 
+// TODO: can thou ripple?
+
 class LaneHeader extends StatelessWidget {
   final EzCP config;
   final AppInfoProvider appInfo;
