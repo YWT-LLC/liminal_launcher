49f4078d0b94a4ec457412fc10869f189e237642
empathictech <105748722+empathictech@users.noreply.github.com>
2026-08-29 08:42:22 -0400

    version bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "llc.ywt.liminal"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
